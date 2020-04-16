d2013ebb6bdd89c311ae9227abc38eb8397953de
Jesse Khorasanee <jessekhorasanee@gmail.com>
2020-04-16 16:48:07 +1200

    Added support to get AWS download URLs from BF discover

--- src/app.py
+++ src/app.py
@@ -4,12 +4,14 @@ from os.path import splitext
 from sanic import Sanic
 from sanic.response import json, html, text, redirect
 from packageIdToScaffold import BFWorker
+from discover import Discover
 import requests
 
 app = Sanic()
 
 logger = logging.getLogger(__name__)
 bfWorker = BFWorker(None)
+discover = Discover()
         
 @app.route('/getInfo')
 async def getInfo(request):
@@ -27,27 +29,33 @@ async def getInfo(request):
 @app.route('/scaffold/<colId:string>/<fileName:string>')
 async def scaffold(request, colId, fileName):
     awsURL = bfWorker.getURLFromCollectionIdAndFileName(colId, splitext(fileName)[0])
-    print(awsURL)
     try:
       resp = requests.get(awsURL)
       return json({'awsURL':resp.url})
+    except Exception as e:
+      return json({'error': e}, status=400)
 
-    except:
-      return json({'error': 'error with the url '}, status=400)
-
-@app.route('discover/<datasetName:int>/<fileName:string>')
-async def discover(request, datasetName, fileName):
-    resp = requests.get('https://api.blackfynn.io/discover/search/files', {'query':fileName, 'datasetId': datasetName})
-    print(resp.json())
+@app.route('/blackfynn/<datasetName:string>/<fileName:string>')
+async def bf_get_file(request, datasetName, fileName):
     try:
-        return json({'awsURL': resp})
-    except:
-        return json({'error': 'error with the url '}, status=400)
+        ds = bfWorker.bf.get_dataset(datasetName)
+        file_url = ds.get_items_by_name('Derivatives')[0].get_items_by_name(fileName)[0].files[0].url
+        return json({'awsURL': file_url}, status=200)
+    except IndexError as e:
+        return json({'error': f'No files found in dataset: {datasetName} of name: {fileName}'}, status=400)
 
-
+@app.route('/discover/<datasetName:string>/<fileName:string>')
+async def get_discover_file(request, datasetName, fileName):
+    try:
+        url = discover.get_file(datasetName, fileName)
+        return json({'awsURL': url}, status=200)
+    except IndexError as e:
+        return json({'error': f'No files found in dataset: {datasetName} of name: {fileName}'}, status=400)
 
 def main():
     app.run(host='0.0.0.0', port=6767)
+
+
 
 
 
