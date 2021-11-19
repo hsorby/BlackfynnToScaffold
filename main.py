dbf5adb0b8459a783747f426a5a67a8716a03682
Jesse Khorasanee <jessekhorasanee@gmail.com>
2021-11-19 16:09:13 +1300

    Fix gunicorn usage

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from app.app import app
+import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 8000)),
+        workers=int(os.environ.get('WEB_CONCURRENCY', 1)),
+        debug=bool(os.environ.get('DEBUG', ''))
+    )
